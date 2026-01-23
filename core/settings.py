3a8e4ca13e8b0ae73c216bf2b5418af629f62c53
Willian Oliveira <willian.dev.tech@gmail.com>
2026-01-23 08:43:43 -0300

    style: stylish creation of the homepage, organization of the SCSS files, and definition of the path where they should be compiled to CSS

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_browser_reload",
     "apps.recipes",
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
